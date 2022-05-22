075891b5bd461095ad4d8ba9bc345bdac48dc98d
Corey <corey.griffith@team.aussiebroadband.com.au>
2022-05-22 19:18:02 +1000

    Rebased to config_model

--- fast2kb/stdafx.h
+++ fast2kb/stdafx.h
@@ -1,14 +1,15 @@
-// stdafx.h : Include file for standard system include files, or
-// project-specific include files that are used frequently, but are changed
-// infrequently.
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
 //
 
 #pragma once
 
 #include "targetver.h"
 
-#include <windows.h>
 #include <stdio.h>
+#include <tchar.h>
 
-#define DIRECTINPUT_VERSION 0x0800
-#include <dinput.h>
+
+
+// TODO: reference additional headers your program requires here
